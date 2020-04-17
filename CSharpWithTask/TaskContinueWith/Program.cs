254faf6e1375d7ec97b89868bb1803306d3100c9
saber <956441594@qq.com>
2020-04-17 12:18:48 +0800

    add "ContinueWith"  function

---
+++ CSharpWithTask/TaskContinueWith/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            //创建一个任务
+            Task<int> task = new Task<int>(() =>
+            {
+                int sum = 0;
+                Console.WriteLine("使用Task执行异步操作.");
+                for (int i = 0; i < 100; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            });
+            //启动任务,并安排到当前任务队列线程中执行任务(System.Threading.Tasks.TaskScheduler)
+            task.Start();
+            Console.WriteLine("主线程执行其他处理");
+            //任务完成时执行处理。
+            Task cwt = task.ContinueWith(t =>
+            {
+                Console.WriteLine("任务完成后的执行结果：{0}", t.Result.ToString());
+            });
+            task.Wait();
+            cwt.Wait();
+        }
+    }
+}
+
